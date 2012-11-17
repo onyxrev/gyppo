d4e378e00a39977f38c68b1d448faa7d1f09ad30
Dan Connor <danconn@danconnor.com>
2012-11-17 03:29:50 -0800

    fixing invalid default config option

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config           = {};
 config.port          = '9000';
 config.uid           = "www";
-config.gid           = config.user;
+config.gid           = config.uid;
 config.transport     = "file";
 config.default_level = "info";
 config.log_file      = "./log/gyppo.log";
